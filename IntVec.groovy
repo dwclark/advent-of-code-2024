d6c8983a67746f8b2be51d5375fd3cde64088b22
David Clark <me@nobody.com>
2024-12-04 22:41:31 -0600

    IntVec + using IntVec on day 04

---
+++ IntVec.groovy
@@ -0,0 +1,86 @@
+import groovy.transform.CompileStatic
+
+class IntVec {
+    private final int[] ary
+    
+    IntVec(int[] ary) {
+	this.ary = ary
+    }
+
+    static IntVec vec(int[] ary) {
+	return new IntVec(ary)
+    }
+
+    private int[] allocate(IntVec rhs) {
+	assert ary.length == rhs.ary.length
+	return new int[ary.length]
+    }
+
+    @Override
+    String toString() { return "(${ary.join(',')})" }
+
+    @Override
+    boolean equals(Object rhs) {
+	if(!(rhs instanceof IntVec))
+	    return false
+
+	return Arrays.equals(ary, ((IntVec) rhs).ary)
+    }
+
+    @Override
+    int hashCode() {
+	return Arrays.hashCode(ary)
+    }
+
+    IntVec plus(IntVec rhs) {
+	int[] tmp = allocate(rhs)
+	for(int i = 0; i < ary.length; ++i)
+	    tmp[i] = ary[i] + rhs.ary[i]
+	return new IntVec(tmp)
+    }
+
+    IntVec minus(IntVec rhs) {
+	final int[] tmp = allocate(rhs)
+	for(int i = 0; i < ary.length; ++i)
+	    tmp[i] = ary[i] - rhs.ary[i]
+	return new IntVec(tmp)
+    }
+
+    IntVec multiply(int rhs) {
+	final int[] tmp = new int[ary.length]
+	for(int i = 0; i < ary.length; ++i)
+	    tmp[i] = ary[i] * rhs
+	return new IntVec(tmp)
+    }
+
+    int getAt(int idx) { return ary[i] }
+
+    int manhattan(IntVec rhs) {
+	assert ary.length == rhs.ary.length
+	int total = 0
+	for(int i = 0; i < ary.length; ++i)
+	    total += Math.abs(ary[i] - rhs.ary[i])
+	return total
+    }
+
+    double rectangular(IntVec rhs) {
+	assert ary.length == rhs.ary.length
+	int total = 0
+	for(int i = 0; i < ary.length; ++i) {
+	    int diff = ary[i] - rhs.ary[i]
+	    total += diff * diff
+	}
+	
+	return Math.sqrt(total)
+    }
+
+    private final static List<IntVec> N_TO_ADD =
+	List.copyOf([vec(-1,-1), vec(-1,0), vec(-1, 1),
+		     vec(0,-1), vec(0,1),
+		     vec(1,-1), vec(1,0), vec(1,1)])
+    
+    List<IntVec> getNeighbors() {
+	assert ary.length == 2
+	return N_TO_ADD.collect { toAdd -> toAdd + this }
+    }
+}
--- day04.groovy
+++ day04.groovy
@@ -1,43 +1,41 @@
 import static Aoc.*
-import static groovy.lang.Tuple.tuple
+import static IntVec.*
 
 
 def part1 = { grid ->
-    def toAdd = [tuple(-1,-1), tuple(-1,0), tuple(-1, 1),
-		 tuple(0,-1), tuple(0,1),
-		 tuple(1,-1), tuple(1,0), tuple(1,1)]
+    final toAdd = [vec(-1,-1), vec(-1,0), vec(-1, 1),
+		   vec(0,-1), vec(0,1),
+		   vec(1,-1), vec(1,0), vec(1,1)]
     
-    def findXmas = { start ->
-	toAdd.sum { tup ->
-	    def letter = { val -> grid[tuple(start[0] + tup[0]*val, start[1] + tup[1]*val)] }
-	    (letter(1) == 'M' && letter(2) == 'A' && letter(3) == 'S') ? 1 : 0
+    def findXmas = { v ->
+	toAdd.sum { a ->
+	    (grid[v + a] == 'M' && grid[v + (a*2)]  == 'A' && grid[v + (a*3)] == 'S') ? 1 : 0
 	}
     }
 
-    grid.inject(0) { total, tup, s -> total += ((s == 'X') ? findXmas(tup) : 0) }
+    grid.inject(0) { total, v, s -> total += ((s == 'X') ? findXmas(v) : 0) }
 }
 
 def part2 = { grid ->
-    def findXmas = { start ->
-	def first = [tuple(-1,-1), tuple(1,1)]
-	def second = [tuple(-1,1), tuple(1,-1)]
-	def letter = { tup -> grid[tuple(tup[0] + start[0], tup[1] + start[1])] }
-	
-	(((letter(first[0]) == 'M' && letter(first[1]) == 'S') ||
-	  (letter(first[0]) == 'S' && letter(first[1]) == 'M')) &&
-	 ((letter(second[0]) == 'M' && letter(second[1]) == 'S') ||
-	  (letter(second[0]) == 'S' && letter(second[1]) == 'M')))
-    }
+    final first = [vec(-1,-1), vec(1,1)]
+    final second = [vec(-1,1), vec(1,-1)]
 
-    grid.inject(0) { total, tup, s -> total += ((s == 'A' && findXmas(tup)) ? 1 : 0) }
+    def findXmas = { v ->
+	(((grid[v + first[0]] == 'M' && grid[v + first[1]] == 'S') ||
+	  (grid[v + first[0]] == 'S' && grid[v + first[1]] == 'M')) &&
+	 ((grid[v + second[0]] == 'M' && grid[v + second[1]] == 'S') ||
+	  (grid[v + second[0]] == 'S' && grid[v + second[1]] == 'M')))
+    }
+    
+    grid.inject(0) { total, v, s -> total += ((s == 'A' && findXmas(v)) ? 1 : 0) }
 }
 
 def grid = [:]
 lines('data/04').eachWithIndex { line, row ->
     line.eachWithIndex { s, col ->
-	grid[tuple(row,col)] = s
+	grid[vec(row,col)] = s
     }
 }
-    
+
 printAssert("Part 1:", part1(grid), 2644,
 	    "Part 2:", part2(grid), 1952)
