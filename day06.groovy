5bf902cd2fda70c03447c54a935b7f99d43b38ac
David Clark <me@nobody.com>
2024-12-06 10:33:01 -0600

    Cleanup and speedup

--- day06.groovy
+++ day06.groovy
@@ -1,28 +1,28 @@
 import static Aoc.*
 import static IntVec.*
+import static java.util.concurrent.CompletableFuture.supplyAsync
 
-final DIRS = [vec(-1,0), vec(0,1), vec(1,0), vec(0,-1)]
+final DIRS = List.copyOf([vec(-1,0), vec(0,1), vec(1,0), vec(0,-1)])
+final lines = new File('data/06').readLines()
 
-def lines = new File('data/06').readLines()
-final numRows = lines.size()
-final numCols = lines[0].length()
-
-def grid = [:]
-def start
-lines.eachWithIndex { line, row ->
-    line.eachWithIndex { s, col ->
-	def ivec = vec(row, col)
-	if(s == '^') {
-	    start = ivec
-	    grid[ivec] = '.'
-	}
-	else {
-	    grid[ivec] = s
+def (grid,start) = { ->
+    def grid = [:]
+    def start
+    lines.eachWithIndex { line, row ->
+	line.eachWithIndex { s, col ->
+	    def ivec = vec(row, col)
+	    if(s == '^') {
+		start = ivec
+		grid[ivec] = '.'
+	    }
+	    else {
+		grid[ivec] = s
+	    }
 	}
     }
-}
 
-grid = Map.copyOf(grid)
+    return [ Map.copyOf(grid), start ]
+}()
 
 def part1 = {
     def turn = 0
@@ -30,71 +30,52 @@ def part1 = {
     def visited = new HashSet([start])
     
     while(grid[current]) {
-	current = current + DIRS[turn % 4]
+	current += DIRS[turn]
 	def at = grid[current]
 	if(at == '.') {
 	    visited.add(current)
 	}
 	else if(at == '#') {
-	    current = current - DIRS[turn % 4]
-	    ++turn
-	}
-	else {
-	    break
+	    current -= DIRS[turn]
+	    turn = (turn + 1) % 4
 	}
     }
 
     return visited.size()
 }
 
-def part2 = {
-    def positions = new HashSet()
-
-    for(int row = 0; row < numRows; ++row) {
-	for(int col = 0; col < numCols; ++col) {
-	    def pos = vec(row, col)
-	    def type = grid[pos]
-	    println pos
-	    if(type == '#' || pos == start)
-		continue
-	    
-	    def newGrid = new LinkedHashMap(grid)
-	    newGrid[pos] = '#'
-
-	    def turn = 0
-	    def current = start
-	    def newVisits = [:]
-	    
-	    while(newGrid[current]) {
-		if(pos == vec(19,7))
-		    println "At 19,7: ${current}"
-		
-		current = current + DIRS[turn % 4]
-		def at = newGrid[current]
-		if(at == '.') {
-		    //do nothing
-		}
-		else if(at == '#') {
-		    def soFar = newVisits.get(current, new HashSet())
-		    if(soFar.contains(DIRS[turn % 4])) {
-			positions.add(pos)
-			break
-		    }
-		    else {
-			soFar.add(DIRS[turn % 4])
-		    }
-		    
-		    current = current - DIRS[turn % 4]
-		    ++turn
-		}
-		else {
-		    break
-		}
+def loopAt = { pos ->
+    def newGrid = grid + [(pos): '#']
+    def turn = 0
+    def current = start
+    def visits = [:]
+    
+    while(newGrid[current]) {
+	current += DIRS[turn]
+	if(newGrid[current] == '#') {
+	    def soFar = visits.get(current, new HashSet())
+	    if(soFar.contains(DIRS[turn])) {
+		return pos
 	    }
+	    else {
+		soFar.add(DIRS[turn])
+	    }
+	    
+	    current -= DIRS[turn]
+	    turn = (turn + 1) % 4
 	}
     }
 
-    return positions.size()
+    return null
 }
 
-println part2()
+def part2 = {
+    grid.inject([]) { results, pos, type ->
+	(type != '#' && pos != start) ? results << supplyAsync(loopAt.curry(pos)) : results
+    }.count { fut ->
+	fut.get()
+    }
+}
+
+printAssert("Part 1:", part1(), 4982,
+	    "Part 2:", part2(), 1663)
