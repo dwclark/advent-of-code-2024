e43eb7d92441bd185f7009a0e38c6ab621473525
David Clark <me@nobody.com>
2024-12-10 09:58:50 -0600

    Refactor to be smaller and express concepts more clearly

--- day10.groovy
+++ day10.groovy
@@ -1,59 +1,35 @@
+import static Aoc.*
 import static IntVec.*
 
 def graph = [:]
 new File('data/10').readLines().eachWithIndex { line, row ->
     line.eachWithIndex { s, col -> graph[vec(row,col)] = s.toInteger() } }
 
-def findTrails(def accum, def graph, def pos, final height) {
-    if(height == 9)
-	accum.add(pos)
+def findPaths(def graph, Closure accumulator, List path) {
+    if(path.size() == 10)
+	accumulator(path)
     else {
+	IntVec pos = path[-1]
+	int height = graph[pos]
 	for(IntVec n in pos.crossNeighbors) {
 	    if(graph.containsKey(n) && graph[n] == height + 1) {
-		findTrails(accum, graph, n, graph[n])
+		findPaths(graph, accumulator, path + [n])
 	    }
 	}
     }
 }
 
-def part1(def graph) {
-    int trailheads
-    graph.each { pos, height ->
+def execute(def graph, def closure) {
+    graph.inject(0) { int total, IntVec pos, int height ->
 	if(height == 0) {
-	    def accum = new HashSet()
-	    findTrails(accum, graph, pos, 0)
-	    trailheads += accum.size()
+	    Set accum = new HashSet()
+	    findPaths(graph, closure.curry(accum), [pos])
+	    total += accum.size()
 	}
-    }
 
-    return trailheads
-}
-
-def findRating(def accum, def soFar, def graph, def pos, final height) {
-    if(height == 9)
-	accum.add(soFar)
-    else {
-	for(IntVec n in pos.crossNeighbors) {
-	    if(graph.containsKey(n) && graph[n] == height + 1) {
-		findRating(accum, soFar + [n], graph, n, graph[n])
-	    }
-	}
+	total
     }
 }
 
-def part2(def graph) {
-    int ratings
-    graph.each { pos, height ->
-	if(height == 0) {
-	    def accum = new HashSet()
-	    findRating(accum, new HashSet([pos]), graph, pos, 0)
-	    ratings += accum.size()
-	}
-    }
-
-    return ratings
-}
-
-
-//println part1(graph)
-println part2(graph)
+printAssert("Part 1:", execute(graph) { Set accum, List path -> accum.add(path[-1]) }, 794,
+	    "Part 2:", execute(graph) { Set accum, List path -> accum.add(path as Set) }, 1706)
