ec1b9412e37ff540e219ef09daaefc88f20647e0
David Clark <me@nobody.com>
2024-12-11 09:10:31 -0600

    Refactored day 11

--- day11.groovy
+++ day11.groovy
@@ -1,9 +1,11 @@
 import static Aoc.*
 import groovy.transform.Immutable
-//def str = "125 17"
-def str = "3279 998884 1832781 517 8 18864 28 0"
-//def str = "3279" //partitioning won't help, looks like num unique values grows slowly
-def vals = str.split(' ') as List
+import groovy.transform.Memoized
+import groovy.transform.Field
+
+@Field final int max = 75
+@Field final str = "3279 998884 1832781 517 8 18864 28 0"
+@Field final vals = str.split(' ')
 
 def blink(def list) {
     def ret = []
@@ -20,62 +22,26 @@ def blink(def list) {
     return ret
 }
 
-def part1(def newVals) {
-    35.times { i ->
-	newVals = blink(newVals)
-	println("${i}: ${(newVals as Set).size()}")
-    }
-
-    return newVals
-}
-
-//def ret = part1(vals)
-
-@Immutable
-class LevelPair {
-    String str
-    int level
-}
-
-def cache = [:]
-def max = 75
-def calculate
-
-calculate = { LevelPair pair ->
-    def found = cache[pair]
-
-    if(found != null) return found
-
-    if(pair.level == max) {
-	cache[pair] = 1G
+@Memoized
+def calculate(String str, int height) {
+    if(height == max)
 	return 1G
+
+    if(str == '0')
+	calculate('1', height + 1)
+    else if(str.length() % 2 == 0) {
+	int mid = str.length().intdiv(2)
+	String first = str.substring(0, mid)
+	String second = str.substring(mid).toBigInteger().toString()
+	return calculate(first, height+1) + calculate(second, height+1)
     }
     else {
-	if(pair.str == '0') {
-	    LevelPair newPair = new LevelPair('1', pair.level+1)
-	    cache[newPair] = calculate(newPair)
-	    return cache[newPair]
-	}
-	else if(pair.str.length() % 2 == 0) {
-	    def v = pair.str
-	    def mid = v.length().intdiv(2)
-	    def first = v.substring(0, mid)
-	    LevelPair pair1 = new LevelPair(first, pair.level+1)
-	    cache[pair1] = calculate(pair1)
-	    def second = v.substring(mid).toBigInteger().toString()
-	    LevelPair pair2 = new LevelPair(second, pair.level+1)
-	    cache[pair2] = calculate(pair2)
-	    return cache[pair1] + cache[pair2]
-	}
-	else {
-	    LevelPair newPair = new LevelPair((2024G * pair.str.toBigInteger()).toString(),
-					      pair.level+1)
-	    cache[newPair] = calculate(newPair)
-	    return cache[newPair]
-	}
+	return calculate((2024G * str.toBigInteger()).toString(), height+1)
     }
 }
 
-def pairs = str.split(' ').collect { new LevelPair(it, 0) }
-println pairs.inject(0G) { tot, pair -> tot += calculate(pair) }
-//println calculate(new LevelPair('0', 0))
+def part1() { (0..<25).toList().inject(vals) { newVals, i -> blink(newVals) }.size() }
+def part2() { vals.inject(0G) { tot, s -> tot += calculate(s, 0) } }
+
+printAssert("Part 1:", part1(), 218956G,
+	    "Part 2:", part2(), 259593838049805G)
