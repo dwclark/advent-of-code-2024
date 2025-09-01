2c530d29321620eb8fc08b1a9a0915a6fdedc5ba
David Clark <me@nobody.com>
2025-09-01 14:13:11 -0500

    Finished day 23

--- day23.groovy
+++ day23.groovy
@@ -1,8 +1,9 @@
 import groovy.transform.Field
-import static Tuple.*
 import static Aoc.*
+import static Tuple.*
 
 @Field final List<String> LINES = new File('data/23').readLines()
+
 @Field final Map<String,Set<String>> CONNECTIONS = LINES.inject([:]) { ret, line ->
     String[] tmp = line.split('-')
     ret.get(tmp[0], new HashSet()).add(tmp[1])
@@ -29,4 +30,34 @@ int containsT(Set<Set<String>> connections) {
     connections.sum { connection -> connection.sum { it.startsWith('t') ? 1 : 0 } ? 1 : 0 }
 }
 
-printAssert("Part1:", containsT(threeConnections()), 1370)
+boolean allConnected(List<String> strings) {
+    for(int i = 0; i < strings.size(); ++i) {
+	for(int j = i+1; j < strings.size(); ++j) {
+	    if(!CONNECTIONS[strings[i]].contains(strings[j])) {
+		return false
+	    }
+	}
+    }
+    
+    return true
+}
+
+Set<String> largest() {
+    List<String> soFar = List.of()
+    CONNECTIONS.each { key, set ->
+	List<String> list = set as List
+	list.subsequences().each { subSeq ->
+	    if(soFar.size() < (subSeq.size() + 1) && allConnected(subSeq))
+		soFar = (subSeq + key).sort()
+	}
+    }
+
+    return soFar
+}
+
+//printAssert("Part1:", containsT(threeConnections()), 1370)
+
+//CONNECTIONS.each { k, v -> println "${k}: ${v}" }
+//println largest()
+//println allConnected(['co', 'de', 'ka', 'ta'])
+println largest().join(',')
