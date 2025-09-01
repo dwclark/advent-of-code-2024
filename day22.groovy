d99017a27849e9c1548d50e46d96801c0b6c6ed6
David Clark <me@nobody.com>
2025-09-01 11:22:04 -0500

    Finished day 22

--- day22.groovy
+++ day22.groovy
@@ -1,6 +1,7 @@
 import groovy.transform.CompileStatic
 import groovy.transform.Field
 import static Aoc.*
+import java.util.concurrent.CompletableFuture
 
 @CompileStatic
 long prune(final long num) {
@@ -13,17 +14,74 @@ long mix(final long secret, final long num) {
 }
 
 @CompileStatic
-long newSecret(final long secret, Integer rounds = 1) {
-    long res = secret
-    rounds.times {
-	res = prune(mix(res, res * 64L))
-	res = prune(mix(res, (long) (res / 32)))
-	res = prune(mix(res, res * 2048L))
+List<Long> secrets(final long start, int rounds = 1) {
+    (0..<rounds).inject([start]) { list, ignore ->
+	long secret = list[-1]
+	secret = prune(mix(secret, secret * 64L))
+	secret = prune(mix(secret, (long) (secret / 32)))
+	list << prune(mix(secret, secret * 2048L))
     }
-
-    return res
 }
 
-@Field final List<Long> all = new File('data/22').readLines().collect { it as Long }
+@CompileStatic
+List<List<Map.Entry<List,Long>>> priceChanges(List<List<Long>> all) {
+    List<List<Map.Entry<List,Long>>> ret = []
+    all.each { List<Long> listSecrets ->
+	List<Long> prices = listSecrets.collect { n -> n % 10 }
+	List<Map.Entry<List,Long>> changes = []
+	for(int i = 4; i < prices.size(); ++i) {
+	    List<Long> diffs = []
+	    for(int j = i-3; j <=i; ++j) {
+		diffs.add(prices[j] - prices[j-1])
+	    }
 
-printAssert("Part 1:", all.sum { newSecret(it, 2000) }, 17577894908L)
+	    long diff = prices[i]
+	    Map.Entry<List,Long> toAdd = Map.entry(diffs, prices[i])
+	    changes.add(toAdd)
+	}
+
+	ret.add(changes)
+    }
+
+    return ret
+}
+
+@CompileStatic
+Long bananas(List<List<Map.Entry<List,Long>>> allChanges) {
+    Set<List> keys = new HashSet<>()
+    allChanges.each { List<Map.Entry<List,Long>> changes ->
+	changes.each { Map.Entry<List,Long> change ->
+	    keys.add(change.key)
+	}
+    }
+
+    List<CompletableFuture<Long>> futures = keys.collect { List key ->
+	CompletableFuture.supplyAsync {
+	    long total = 0L
+	    
+	    allChanges.each { List<Map.Entry<List,Long>> changes ->
+		for(Map.Entry<List,Long> change : changes) {
+		    List diffs = change.key
+		    Long price = change.value
+		    if(diffs == key) {
+			total += price
+			break
+		    }
+		}
+	    }
+	    
+	    return total
+	}
+    }
+
+    futures.collect { f -> f.get() }.max()
+}
+
+@Field final List<Long> initialSecrets = new File('data/22').readLines().collect { it as Long }
+
+List<List<Long>> allSecrets = initialSecrets.inject([]) { list, num -> list << secrets(num, 2000) }
+List<List<Map.Entry<List,Long>>> changes = priceChanges(allSecrets)
+printAssert("Part 1:", allSecrets.sum { list -> list[-1] }, 17577894908L,
+	    "Part 2:", bananas(changes), 1931L)
+
+
